0444d4cd709dd1664f327d07beaa5362810afda3
Jack Savage <jacksav@protonmail.com>
2020-10-11 08:44:40 -0700

    Adds ComplementCode method

--- SegmentationART/Util.cs
+++ SegmentationART/Util.cs
@@ -1,6 +1,18 @@
-﻿namespace SegmentationART
+﻿using System;
+
+namespace SegmentationART
 {
     internal static class Util
     {
+        public static double[] ComplementCode(this double[] x)
+        {
+            var result = new double[2 * x.Length];
+            Array.Copy(x, 0, result, 0, x.Length);
+
+            for (var i = 0; i < x.Length; i++)
+                result[i + x.Length] = 1.0 - x[i];
+
+            return result;
+        }
     }
 }
