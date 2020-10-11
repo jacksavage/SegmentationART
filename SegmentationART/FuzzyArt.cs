0ad171d7dbd4534e08ee339077c90a7ba361d000
Jack Savage <jacksav@protonmail.com>
2020-10-11 10:48:33 -0700

    Implaments FuzzyArt Learn method and constructor

--- SegmentationART/FuzzyArt.cs
+++ SegmentationART/FuzzyArt.cs
@@ -1,6 +1,73 @@
-﻿namespace SegmentationART
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SegmentationART
 {
     internal class FuzzyArt
     {
+        public List<double[]> Nodes { get; } = new();
+        public int InputLength { get; }
+        public double Choice { get; }
+        public double LearningRate { get; }
+        public double Vigilance { get; }
+
+        public FuzzyArt(int inputLength, double choice, double learningRate, double vigilance)
+        {
+            // check that params are in proper ranges
+            // todo replace with logging
+            if (inputLength < 1) throw new ArgumentOutOfRangeException(nameof(inputLength));
+            if (choice <= 0) throw new ArgumentOutOfRangeException(nameof(choice));
+            if (learningRate < 0 || learningRate > 1) throw new ArgumentOutOfRangeException(nameof(learningRate));
+            if (vigilance < 0 || vigilance > 1) throw new ArgumentOutOfRangeException(nameof(vigilance));
+
+            // add an uncommitted node
+            // the last node will always be considered uncommitted
+            Nodes.Add(Util.Ones(inputLength));
+
+            // store the params for reference later
+            InputLength = inputLength;
+            Choice = choice;
+            LearningRate = learningRate;
+            Vigilance = vigilance;
+        }
+
+        public int Learn(double[] input)
+        {
+            // check input length
+            // todo replace exception with log message
+            if (input.Length != InputLength) throw new ArgumentException(null, nameof(input));
+
+            // complement code the input
+            var codedInput = input.ComplementCode();
+
+            // find a node to learn this input
+            // the 'First' function will throw if none of the nodes pass the vigilance criterion
+            // however, this should not occur bc the final node is "uncommitted" and should always pass
+            var learnerQuery =
+                from id in Enumerable.Range(0, Nodes.Count)
+                let weights = Nodes[id]
+                let fuzInt = codedInput.FuzzyIntersection(weights)
+                let fuzIntNorm = fuzInt.CityBlockNorm()
+                orderby fuzIntNorm / (Choice + weights.CityBlockNorm())
+                where fuzIntNorm / codedInput.CityBlockNorm() >= Vigilance
+                select (id, weights, fuzInt);
+            var learner = learnerQuery.First();
+
+            // if the uncommitted node was selected then a "reset" occurs
+            // this means a new uncommitted node must be added
+            if (learner.id == Nodes.Count - 1) Nodes.Add(Util.Ones(InputLength));
+
+            // learn the input
+            for (int i = 0; i < learner.weights.Length; i++)
+            {
+                learner.weights[i] =
+                    (1 - LearningRate) * learner.weights[i] +
+                    LearningRate * learner.fuzInt[i];
+            }
+
+            // return the ID (code) for the node which learned the input
+            return learner.id;
+        }
     }
 }
