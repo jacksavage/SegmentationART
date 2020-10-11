61025c65575c5d0c1ff7b8f498604617cdf82a4c
Jack <jacksavage@users.noreply.github.com>
2020-10-11 13:02:48 -0700

    Implements WorkMemory Reset, Add, and constructor

--- SegmentationART/WorkingMemory.cs
+++ SegmentationART/WorkingMemory.cs
@@ -1,6 +1,32 @@
-﻿namespace SegmentationART
+﻿using System;
+
+namespace SegmentationART
 {
     internal class WorkingMemory
     {
+        public double[] Output { get; }
+        private readonly double Q;
+
+        public WorkingMemory(int length, double q)
+        {
+            Output = new double[length];
+            Q = q;
+        }
+
+        public void Reset() => 
+            Array.Clear(Output, 0, Output.Length);
+
+        public void Add(int index)
+        {
+            // will throw if index is out of range
+            // this means our working memory is not large enough to represent
+            // all of the states from the previous ART network
+            // todo could make this a dynamic data structure, but that would make
+            // the following ART network input dynamic (requiring the length of
+            // its node weights to be dynamic)
+
+            // note: index = r - 1
+            Output[index] = Math.Pow(Q, index);
+        }
     }
 }
