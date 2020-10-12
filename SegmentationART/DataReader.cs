cf223a71674e6e08a9e783d33ffcf76ca8d0f070
Jack Savage <jacksav@protonmail.com>
2020-10-11 20:46:59 -0700

    Adds DataReader class and ReadWords method

--- SegmentationART/DataReader.cs
+++ SegmentationART/DataReader.cs
@@ -1,6 +1,41 @@
-﻿namespace SegmentationART
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace SegmentationART
 {
     internal class DataReader
     {
+        private record Phoneme(char ID, double[] Vector);
+
+        private static IEnumerable<Phoneme> ReadPhonemes(string path) =>
+            from line in File.ReadLines(path)
+            let fields = line.Split("\t")
+            let id = string.IsNullOrWhiteSpace(fields[1]) ? fields[0] : fields[1]
+            let vector = fields.Skip(2).Select(f => double.Parse(f)).ToArray()
+            select new Phoneme(id[0], vector);
+
+        private record Word(string ID, string Phonemes);
+
+        private static IEnumerable<Word> ReadWords(string path) =>
+            from line in File.ReadLines(path)
+            let fields = line.Split("\t")
+            select new Word(fields[0], fields[1]);
+
+        public static Dictionary<string, double[][]> ReadWords(string phonemePath, string wordPath)
+        {
+            var phonemes =
+                ReadPhonemes(phonemePath)
+                .ToDictionary(p => p.ID, p => p.Vector);
+
+            var words =
+                ReadWords(wordPath)
+                .ToDictionary(
+                    w => w.ID,
+                    w => w.Phonemes.Select(p => phonemes[p].ComplementCode()).ToArray()
+                );
+
+            return words;
+        }
     }
 }
