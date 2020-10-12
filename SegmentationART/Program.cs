3614a4bc271d42fa6a19e29c861a9658ab246fd7
Jack Savage <jacksav@protonmail.com>
2020-10-11 20:48:02 -0700

    Loads data and instantiates a phoneme-ART module

--- SegmentationART/Program.cs
+++ SegmentationART/Program.cs
@@ -1,3 +1,16 @@
-﻿using System;
+﻿using SegmentationART;
 
-Console.WriteLine("Hello World!");
+var wordPhonemes = 
+    DataReader.ReadWords(
+        phonemePath: @"data\phonemes.tsv", 
+        wordPath: @"data\words.tsv"
+    );
+
+var phonemeArt =
+    new FuzzyArt(
+        inputLength: 14,    // input vectors are 7 dimensional and complement coded
+        choice: 0.1,        // ?
+        learningRate: 1.0,  // fast learning
+        vigilance: 0.95     // one node per phoneme
+    );
+
